fe10c4ed8c5ef824ade23dc36ecdb5f2fbb166bd
Chimivmc777 <chimininja777@gmail.com>
2026-08-05 22:10:05 +0530

    Prepared app for Render deployment

--- app.py
+++ app.py
@@ -3634,6 +3634,5 @@ def admin_logout():
     flash("Logged out successfully!", "success")
     return redirect("/")
 # ================= RUN ================= #
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
