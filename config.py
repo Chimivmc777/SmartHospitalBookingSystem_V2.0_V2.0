4fe1c7060c62408ed45b2d46b68650d61bab23bf
Chimivmc777 <chimininja777@gmail.com>
2026-08-08 03:20:01 +0530

    Fix Railway database environment

--- config.py
+++ config.py
@@ -5,12 +5,11 @@ load_dotenv()
 
 
 class Config:
-
-    MYSQL_HOST = os.getenv("MYSQLHOST") or os.getenv("MYSQL_HOST")
-    MYSQL_PORT = int(os.getenv("MYSQLPORT") or os.getenv("MYSQL_PORT", "3306"))
-    MYSQL_USER = os.getenv("MYSQLUSER") or os.getenv("MYSQL_USER")
-    MYSQL_PASSWORD = os.getenv("MYSQLPASSWORD") or os.getenv("MYSQL_PASSWORD")
-    MYSQL_DB = os.getenv("MYSQLDATABASE") or os.getenv("MYSQL_DATABASE")
+    MYSQL_HOST = os.getenv("MYSQLHOST")
+    MYSQL_PORT = int(os.getenv("MYSQLPORT", "3306"))
+    MYSQL_USER = os.getenv("MYSQLUSER")
+    MYSQL_PASSWORD = os.getenv("MYSQLPASSWORD")
+    MYSQL_DB = os.getenv("MYSQLDATABASE")
 
     SECRET_KEY = os.getenv(
         "SECRET_KEY",
