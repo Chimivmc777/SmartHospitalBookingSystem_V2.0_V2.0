26354a57fc6e908f2edb43d8af5b894c30880b8c
Chimivmc777 <chimininja777@gmail.com>
2026-08-08 02:36:06 +0530

    Fix Railway MySQL environment configuration

--- config.py
+++ config.py
@@ -6,14 +6,12 @@ load_dotenv()
 
 class Config:
 
-    # Railway MySQL
-    MYSQL_HOST = os.getenv("MYSQL_HOST")
-    MYSQL_PORT = int(os.getenv("MYSQL_PORT", "3306"))
-    MYSQL_USER = os.getenv("MYSQL_USER")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-    MYSQL_DB = os.getenv("MYSQL_DATABASE")
+    MYSQL_HOST = os.getenv("MYSQLHOST") or os.getenv("MYSQL_HOST")
+    MYSQL_PORT = int(os.getenv("MYSQLPORT") or os.getenv("MYSQL_PORT", "3306"))
+    MYSQL_USER = os.getenv("MYSQLUSER") or os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQLPASSWORD") or os.getenv("MYSQL_PASSWORD")
+    MYSQL_DB = os.getenv("MYSQLDATABASE") or os.getenv("MYSQL_DATABASE")
 
-    # Flask secret key
     SECRET_KEY = os.getenv(
         "SECRET_KEY",
         "ehealthbhutan2026"
